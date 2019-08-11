2dc53d15d06b3a5d215de240eca9114a0d95460c
Russell Waite <russellwaite@gmail.com>
2019-08-11 21:33:30 +0100

    altering flow to wait for helm to do its thing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kindistiolab
 just a way to repeatedly setup my lab for learning more about istio
+
+tip - run script with:
+
+. ./setup.sh 
+
+to then set the env variables into your current session - including the generated password for openfaas. the first dot is shorthand for source.
--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-#! zsh
-
-export ISTIO_VERSION="1.2.2"
-export ISTIO_FOLDER="istio-$ISTIO_VERSION"
-
 # install kind if not present
 if ! hash kind 2>/dev/null; then
     GO111MODULE="on" go get sigs.k8s.io/kind@v0.4.0
@@ -17,6 +12,10 @@ kind create cluster --config ./one_worker.yaml
 export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
 kubectl cluster-info
 
+#---------------------------------------------------------------------------------------
+export ISTIO_VERSION="1.2.2"
+export ISTIO_FOLDER="istio-$ISTIO_VERSION"
+
 # download istio if required
 [[ ! -d "./$ISTIO_FOLDER" ]] && curl -L https://git.io/getLatestIstio | sh -
 
@@ -29,9 +28,9 @@ kubectl cluster-info
 
 # install istio - without helm (seems pointless now as helm's being installed a little later on...)
 until [ -d "$ISTIO_FOLDER" ]; do sleep 1; done
-cd $ISTIO_FOLDER
+
 # CRDs for istio
-for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
+for i in $ISTIO_FOLDER/install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
 
 # TODO: what to wait on here? do we even need to?
 
@@ -40,7 +39,7 @@ kubectl label namespace default istio-injection=enabled
 #gets slow from here on in - downloading ALOT - TODO: need to look at storing and retrieving from local docker store/forcing images into Kind
 
 # istio install demo setup but without mTLS - damn near everything turned on...
-kubectl apply -f install/kubernetes/istio-demo.yaml
+kubectl apply -f $ISTIO_FOLDER/install/kubernetes/istio-demo.yaml
 
 # setup some env vars that might become useful
 read INGRESS_HOST <<<$( kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}' )
@@ -48,20 +47,24 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 
-#setup port forwarding for istio tools kiali, prometheus, grafana, jaegar
+export PORT_FWD_IP=0.0.0.0 # 127.0.0.1
+
+#setup port forwarding for istio tools kiali, prometheus, grafana, jaegar - MASSIVELY insecure thanks to me binding to all IPS using 0.0.0.0
+# you'll need to allow these ports through if you want to go out of the bow - sudo ufw allow <port> e.g. sudo ufw allow 20001
 until kubectl get pod -l app=kiali -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
-kubectl port-forward -n istio-system svc/kiali 20001:20001 &
+kubectl port-forward -n istio-system svc/kiali 20001:20001 --address $PORT_FWD_IP &
 until kubectl get pod -l app=grafana -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
-kubectl port-forward -n istio-system svc/grafana 3000:3000 &
+kubectl port-forward -n istio-system svc/grafana 3000:3000 --address $PORT_FWD_IP &
 until kubectl get pod -l app=prometheus -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
-kubectl port-forward -n istio-system svc/prometheus 9090:9090 &
+kubectl port-forward -n istio-system svc/prometheus 9090:9090 --address $PORT_FWD_IP &
 until kubectl get pod -l app=jaeger -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
-kubectl port-forward -n istio-system svc/tracing 10080:80 &
+kubectl port-forward -n istio-system svc/tracing 10080:80 --address $PORT_FWD_IP &
 
-#install bookinfo demo
-kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
-kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+#install istio bookinfo demo
+kubectl apply -f $ISTIO_FOLDER/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f $ISTIO_FOLDER/samples/bookinfo/networking/bookinfo-gateway.yaml
 
+#---------------------------------------------------------------------------------------
 # install helm into the cluster (I'm not a fan but it makes things a bit easier for now)
 # this needs the helm client installed locally...
 if ! hash helm 2>/dev/null; then
@@ -72,21 +75,34 @@ if ! hash helm 2>/dev/null; then
 fi
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-
-until kubectl get pod -l app=helm -l name=tiller -n kube-system | grep -m 1 "Running"; do sleep 1 ; done
+#kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 helm init --service-account tiller --upgrade
 
+
+#---------------------------------------------------------------------------------------
 # create openfaas namespaces (just running something fresh from the internet in my local cluster, no reason to ... PANIC!!!)
 kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml
 kubectl get namespaces --show-labels
-
 
 # and again - install blind from internet - HELM - magic... (oh, this is openfaas install by the way)
 helm repo add openfaas https://openfaas.github.io/faas-netes/
 export OF_PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
 kubectl -n openfaas create secret generic basic-auth --from-literal=basic-auth-user=admin --from-literal=basic-auth-password="$OF_PASSWORD"
 helm repo update
+echo "Waiting for tiller pod to come online..."
+until kubectl get pod -l app=helm -l name=tiller -n kube-system | grep -m 1 "1/1"; do sleep 1 ; done
 helm upgrade openfaas --install openfaas/openfaas --namespace openfaas --set basic_auth=true --set functionNamespace=openfaas-fn
 
 kubectl --namespace=openfaas get deployments -l "release=openfaas, app=openfaas"
+kubectl get svc -n openfaas gateway-external -o wide
+echo "Waiting for openfaas pod to come online..."
+until kubectl get pod -n openfaas -l app=gateway | grep -m 1 "Running"; do sleep 1 ; done
+kubectl port-forward -n openfaas svc/gateway 31112:31112 --address $PORT_FWD_IP 2>/dev/null &
+
+# allowing holes in the hosts firewall so externals can communicate with the port forwarded services 
+# here be dragons...
+# sudo ufw allow 20001
+# sudo ufw allow 3000
+# sudo ufw allow 9090
+# sudo ufw allow 10080
+# sudo ufw allow 31112
