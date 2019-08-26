00abeb529644b8d02d21789fdcbe350e156f6bc6
Russell Waite <russellwaite@gmail.com>
2019-08-26 13:25:49 +0100

    fixed typo

--- kind_load_images.sh
+++ kind_load_images.sh
@@ -1,7 +1,7 @@
 # this file holds all of the images we downloaded with pull_images.sh
 export INDEX_FILE="image_list.txt"
 
-# pull_images.sh shoudl have loaded them into the host Docker, we now need to load these into Kind
+# pull_images.sh should have loaded them into the host Docker, we now need to load these into Kind
 input=$INDEX_FILE
 while IFS= read -r line
 do
