8fd2fd90795526e44265e0d83764f4e6715a0954
Russell Waite <russellwaite@gmail.com>
2019-08-26 13:08:57 +0100

    tiny cleanup - align comment start, kill dead code There is some commented out code there for me to learn

--- setup.sh
+++ setup.sh
@@ -17,12 +17,11 @@ if (($SHOULD_ALLOW_EXTERNAL_ACCESS)); then
     sudo ufw allow $OPENFAAS_PORT
 else
     export PORT_FWD_IP=127.0.0.1
-    sudo ufw deny $KIALI_PORT
-    sudo ufw deny $GRAFANA_PORT
-    sudo ufw deny $PROMETHEUS_PORT
-    sudo ufw deny $JAEGER_PORT
-    sudo ufw deny $OPENFAAS_PORT
-
+    sudo ufw delete allow $KIALI_PORT
+    sudo ufw delete allow $GRAFANA_PORT
+    sudo ufw delete allow $PROMETHEUS_PORT
+    sudo ufw delete allow $JAEGER_PORT
+    sudo ufw delete allow $OPENFAAS_PORT
 fi
 
 export ISTIO_VERSION="1.2.2"
@@ -52,12 +51,9 @@ echo "Loading $line image from the docker image cache... (docker images)"
   kind load docker-image $line
 done < "$input"
 
-
 #---------------------------------------------------------------------------------------
-
 # download istio if required
 [[ ! -d "./$ISTIO_FOLDER" ]] && curl -L https://git.io/getLatestIstio | sh -
-
 
 # this repeadedly calls a command until it sees the desired output
 #until my_cmd | grep -m 1 "String Im Looking For"; do : ; done
@@ -72,12 +68,9 @@ until [ -d "$ISTIO_FOLDER" ]; do sleep 1; done
 # CRDs for istio
 for i in $ISTIO_FOLDER/install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
 
-# TODO: what to wait on here? do we even need to?
-
 kubectl label namespace default istio-injection=enabled
 
-#gets slow from here on in - downloading ALOT - TODO: need to look at storing and retrieving from local docker store/forcing images into Kind
-
+# gets slow from here on in - potentially (see pull-images.sh) downloading ALOT
 # istio install demo setup but without mTLS - damn near everything turned on...
 kubectl apply -f $ISTIO_FOLDER/install/kubernetes/istio-demo.yaml
 
@@ -87,9 +80,7 @@ export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
 export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
 
-
-
-#setup port forwarding for istio tools kiali, prometheus, grafana, jaegar - MASSIVELY insecure thanks to me binding to all IPS using 0.0.0.0
+# setup port forwarding for istio tools kiali, prometheus, grafana, jaegar - MASSIVELY insecure thanks to me binding to all IPS using 0.0.0.0
 # you'll need to allow these ports through if you want to go out of the bow - sudo ufw allow <port> e.g. sudo ufw allow 20001
 echo "Waiting for Kiali pod to be ready..."
 until kubectl get pod -l app=kiali -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
@@ -107,7 +98,7 @@ echo "Waiting for Jaeger pod to be ready..."
 until kubectl get pod -l app=jaeger -n istio-system | grep -m 1 "Running"; do sleep 1 ; done
 kubectl port-forward -n istio-system svc/tracing $JAEGER_PORT:80 --address $PORT_FWD_IP &
 
-#install istio bookinfo demo
+# install istio bookinfo demo
 kubectl apply -f $ISTIO_FOLDER/samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f $ISTIO_FOLDER/samples/bookinfo/networking/bookinfo-gateway.yaml
 
@@ -122,9 +113,7 @@ if ! hash helm 2>/dev/null; then
 fi
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-#kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 helm init --service-account tiller --upgrade
-
 
 #---------------------------------------------------------------------------------------
 # create openfaas namespaces (just running something fresh from the internet in my local cluster, no reason to ... PANIC!!!)
